5ca66666cd8f6ca3d381c95bf631ea07bf96db44
answel <answelc@gmail.com>
2018-03-06 17:14:55 +0800

    :trollface:

--- src/TagCategoryTrait.php
+++ src/TagCategoryTrait.php
@@ -3,14 +3,12 @@
 namespace Viviniko\Tag;
 
 use Illuminate\Support\Facades\Config;
-use Viviniko\Tag\Models\TagCategory;
 
 trait TagCategoryTrait
 {
     public function tags()
     {
         return $this->morphToMany(Config::get('tag.tag'), 'category', Config::get('tag.tag_category_table'))
-            ->using(TagCategory::class)
             ->withPivot(['category_id', 'category_type', 'tag_id', 'url_rewrite', 'meta_title', 'meta_keywords', 'meta_description'])
             ->select(Config::get('tag.tags_table') . '.*');
     }
