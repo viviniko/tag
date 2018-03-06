c21aad7e748f726513666aa1688c6e3700d0ecad
answel <answelc@gmail.com>
2018-03-06 17:56:16 +0800

    :trollface:

--- src/CategoryTagTrait.php
+++ src/CategoryTagTrait.php
@@ -9,6 +9,6 @@ trait CategoryTagTrait
     public function tags()
     {
         return $this->morphMany(Config::get('tag.category_tag'), 'category', Config::get('tag.tag_category_table'))
-            ->select(Config::get('tag.category_tag') . '.*');
+            ->select(Config::get('tag.tag_category_table') . '.*');
     }
 }
