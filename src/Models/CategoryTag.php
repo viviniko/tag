411174bfe2898b53dc5018246f1aeb88f6acb599
answel <answelc@gmail.com>
2018-03-06 18:28:34 +0800

    :trollface:

--- src/Models/CategoryTag.php
+++ src/Models/CategoryTag.php
@@ -7,6 +7,8 @@ use Viviniko\Support\Database\Eloquent\Model;
 
 class CategoryTag extends Model
 {
+    public $timestamps = false;
+
     protected $tableConfigKey = 'tag.tag_category_table';
 
     protected $fillable = ['category_id', 'category_type', 'tag_id', 'url_rewrite', '', 'meta_title', 'meta_keywords', 'meta_description'];
