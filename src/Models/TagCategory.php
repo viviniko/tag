360b4cf705c2b49b17f66d9105ae176fee8fcac1
answel <answelc@gmail.com>
2018-03-06 16:37:59 +0800

    :trollface:

---
+++ src/Models/TagCategory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Viviniko\Tag\Models;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class TagCategory extends Pivot
+{
+    public $timestamps = false;
+
+    protected $fillable = [
+        'category_id', 'category_type', 'tag_id', 'url_rewrite', 'meta_title', 'meta_keywords', 'meta_description'
+    ];
+}
--- src/TagCategoryTrait.php
+++ src/TagCategoryTrait.php
@@ -3,11 +3,15 @@
 namespace Viviniko\Tag;
 
 use Illuminate\Support\Facades\Config;
+use Viviniko\Tag\Models\TagCategory;
 
 trait TagCategoryTrait
 {
     public function tags()
     {
-        return $this->morphToMany(Config::get('tag.tag'), 'category', Config::get('tag.tag_category_table'))->select(Config::get('tag.tags_table') . '.*');
+        return $this->morphToMany(Config::get('tag.tag'), 'category', Config::get('tag.tag_category_table'))
+            ->using(TagCategory::class)
+            ->withPivot(['category_id', 'category_type', 'tag_id', 'url_rewrite', 'meta_title', 'meta_keywords', 'meta_description'])
+            ->select(Config::get('tag.tags_table') . '.*');
     }
 }
