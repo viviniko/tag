5ba11bd16a2a7e99a7dec53a739322a6cb8c8d3f
answel <answelc@gmail.com>
2018-03-06 17:47:31 +0800

    :trollface:

---
+++ src/CategoryTagTrait.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Viviniko\Tag;
+
+use Illuminate\Support\Facades\Config;
+
+trait CategoryTagTrait
+{
+    public function tags()
+    {
+        return $this->morphMany(Config::get('tag.category_tag'), 'category', Config::get('tag.tag_category_table'))
+            ->select(Config::get('tag.category_tag') . '.*');
+    }
+}
---
+++ src/Models/CategoryTag.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Viviniko\Tag\Models;
+
+use Illuminate\Support\Facades\Config;
+use Viviniko\Support\Database\Eloquent\Model;
+
+class CategoryTag extends Model
+{
+    protected $tableConfigKey = 'tag.tag_category_table';
+
+    protected $fillable = ['category_id', 'category_type', 'tag_id', 'url_rewrite', '', 'meta_title', 'meta_keywords', 'meta_description'];
+
+    public function tag()
+    {
+        return $this->belongsTo(Config::get('tag.tag'), 'tag_id');
+    }
+
+    public function getNameAttribute()
+    {
+        return $this->tag->name;
+    }
+
+    public function category()
+    {
+        return $this->morphTo();
+    }
+}
