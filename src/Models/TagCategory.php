8290294442000421642797d76ba45ffb98cfe2a6
answel <answelc@gmail.com>
2018-03-06 17:05:22 +0800

    :trollface:

--- src/Models/TagCategory.php
+++ src/Models/TagCategory.php
@@ -11,4 +11,9 @@ class TagCategory extends Pivot
     protected $fillable = [
         'category_id', 'category_type', 'tag_id', 'url_rewrite', 'meta_title', 'meta_keywords', 'meta_description'
     ];
+
+    public function category()
+    {
+        return $this->morphTo();
+    }
 }
